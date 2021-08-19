83386c7f4e7b8c8493025fdc53960d33c8c05d93
MartinSStewart <MartinSStewart@gmail.com>
2021-08-19 11:37:34 +0200

    Remove unused dependencies

--- A.elm
+++ A.elm
@@ -8,24 +8,24 @@ englishTexts =
         }
     , success =
         { checkYourEmail = "Check your e-mail"
-        , emailSent = \email -> "We have sent an e-mail to **" ++ Email.toString email ++ """**.
+        , emailSent = \email -> "We have sent an e-mail to **" ++ Email.toString email ++ "**.
 
-Click the button in the email to log in."""
+Click the button in the email to log in."
         }
     , emailSentInstructions = "If you didn’t receive an e-mail from us, check your **Spam folder**, and make sure that the e-mail address is correctly entered."
     }
-
+    
 swedishTexts =
     { form =
-        { emailAddressNotValid = "(Swedish) This email is not valid"
+        { emailAddressNotValid = "(Swedish) This email is not valid123123\n"
         , emailAddressText = "(Swedish) E-mail"
         , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
         }
     , success =
         { checkYourEmail = "(Swedish) Check your e-mail"
-        , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
+        , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ "**.
 
-Click the button in the email to log in."""
+Click the button in the email to log in."
         }
     , emailSentInstructions = "(Swedish) If you didn’t receive an e-mail from us, check your **Spam folder**, and make sure that the e-mail address is correctly entered."
     }
