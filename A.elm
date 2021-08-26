7869a5eaeceb89a5eef379a46edc167d0c6d4bba
MartinSStewart <MartinSStewart@gmail.com>
2021-08-26 14:41:33 +0200

    Update A.elm

--- A.elm
+++ A.elm
@@ -1,227 +1,80 @@
 module A exposing (..)
 
+
 englishTexts =
     { form =
         { emailAddressNotValid = "This email is not valid"
         , emailAddressText = "E-mail"
         , termsOfService = \urls -> "By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
         }
-    , success =
-        { checkYourEmail = "Check your e-mail"
-        , emailSent = \email -> "We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-Click the button in the email to log in."""
+    , form3 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail17"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
         }
-    , emailSentInstructions = "If you didn’t receive an e-mail from us, check your **Spam folder**, and make sure that the e-mail address is correctly entered."
-        , form2 =
-                  { emailAddressNotValid = "(Swedish) This email is not valid"
-                  , emailAddressText = "(Swedish) E-mail"
-                  , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                  }
-              , success =
-                  { checkYourEmail = "(Swedish) Check your e-mail"
-                  , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
+    , form4 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail15"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+        }
+    , form5 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail13"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+        }
+    , form6 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail11"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+        }
+    }
 
-          Click the button in the email to log in."""
-                  }
-        , form3 =
-                  { emailAddressNotValid = "(Swedish) This email is not valid"
-                  , emailAddressText = "(Swedish) E-mail"
-                  , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                  }
-              , success =
-                  { checkYourEmail = "(Swedish) Check your e-mail"
-                  , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-          Click the button in the email to log in."""
-                  , form3 =
-                                  { emailAddressNotValid = "(Swedish) This email is not valid"
-                                  , emailAddressText = "(Swedish) E-mail"
-                                  , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                                  }
-                              , success =
-                                  { checkYourEmail = "(Swedish) Check your e-mail"
-                                  , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                          Click the button in the email to log in."""
-                                  }
-                  }
-        , form4 =
-                       { emailAddressNotValid = "(Swedish) This email is not valid"
-                       , emailAddressText = "(Swedish) E-mail"
-                       , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                       }
-                   , success =
-                       { checkYourEmail = "(Swedish) Check your e-mail"
-                       , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-               Click the button in the email to log in."""
-                       , form3 =
-                                       { emailAddressNotValid = "(Swedish) This email is not valid"
-                                       , emailAddressText = "(Swedish) E-mail"
-                                       , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                                       }
-                                   , success =
-                                       { checkYourEmail = "(Swedish) Check your e-mail"
-                                       , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                               Click the button in the email to log in."""
-                                       }
-                       }
-        , form5 =
-                           { emailAddressNotValid = "(Swedish) This email is not valid"
-                           , emailAddressText = "(Swedish) E-mail"
-                           , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                           }
-                       , success =
-                           { checkYourEmail = "(Swedish) Check your e-mail"
-                           , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                   Click the button in the email to log in."""
-                           , form3 =
-                                           { emailAddressNotValid = "(Swedish) This email is not valid"
-                                           , emailAddressText = "(Swedish) E-mail"
-                                           , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                                           }
-                                       , success =
-                                           { checkYourEmail = "(Swedish) Check your e-mail"
-                                           , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                                   Click the button in the email to log in."""
-                                           }
-                           }
-       , form6 =
-                              { emailAddressNotValid = "(Swedish) This email is not valid"
-                              , emailAddressText = "(Swedish) E-mail"
-                              , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                              }
-                          , success =
-                              { checkYourEmail = "(Swedish) Check your e-mail"
-                              , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                      Click the button in the email to log in."""
-                              , form3 =
-                                              { emailAddressNotValid = "(Swedish) This email is not valid"
-                                              , emailAddressText = "(Swedish) E-mail"
-                                              , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                                              }
-                                          , success =
-                                              { checkYourEmail = "(Swedish) Check your e-mail"
-                                              , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                                      Click the button in the email to log in."""
-                                              }
-                              }
-    } 
 
 swedishTexts =
     { form =
         { emailAddressNotValid = "(Swedish) This email is not valid"
-        , emailAddressText = "(Swedish) E-mail"
+        , emailAddressText = "(Swedish) E-mail9"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+        }
+    , emailSentInstructions = "(Swedish) If you didn’t receive an e-mail from us, check your **Spam folder**, and make sure that the e-mail address is correctly entered."
+    , form2 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail8"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+        }
+    , form3 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail7"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+        }
+    , form4 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail5"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+        }
+    , form5 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail3"
+        , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+        }
+    , form6 =
+        { emailAddressNotValid = "(Swedish) This email is not valid"
+        , emailAddressText = "(Swedish) E-mail1"
         , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
         }
     , success =
         { checkYourEmail = "(Swedish) Check your e-mail"
         , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
 
-Click the button in the email to log in."""
+                 Click the button in the email to log in."""
+        , form3 =
+            { emailAddressNotValid = "(Swedish) This email is not valid"
+            , emailAddressText = "🚧"
+            , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
+            }
+        , success =
+            { checkYourEmail = ""
+            , emailSent = \email -> Email.toString email
+            }
         }
-    , emailSentInstructions = "(Swedish) If you didn’t receive an e-mail from us, check your **Spam folder**, and make sure that the e-mail address is correctly entered."
-    , form2 =
-              { emailAddressNotValid = "(Swedish) This email is not valid"
-              , emailAddressText = "(Swedish) E-mail"
-              , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-              }
-          , success =
-              { checkYourEmail = "(Swedish) Check your e-mail"
-              , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-      Click the button in the email to log in."""
-              }
-    , form3 =
-              { emailAddressNotValid = "(Swedish) This email is not valid"
-              , emailAddressText = "(Swedish) E-mail"
-              , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-              }
-          , success =
-              { checkYourEmail = "(Swedish) Check your e-mail"
-              , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-      Click the button in the email to log in."""
-              , form3 =
-                              { emailAddressNotValid = "(Swedish) This email is not valid"
-                              , emailAddressText = "(Swedish) E-mail"
-                              , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                              }
-                          , success =
-                              { checkYourEmail = "(Swedish) Check your e-mail"
-                              , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                      Click the button in the email to log in."""
-                              }
-              }
-    , form4 =
-                   { emailAddressNotValid = "(Swedish) This email is not valid"
-                   , emailAddressText = "(Swedish) E-mail"
-                   , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                   }
-               , success =
-                   { checkYourEmail = "(Swedish) Check your e-mail"
-                   , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-           Click the button in the email to log in."""
-                   , form3 =
-                                   { emailAddressNotValid = "(Swedish) This email is not valid"
-                                   , emailAddressText = "(Swedish) E-mail"
-                                   , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                                   }
-                               , success =
-                                   { checkYourEmail = "(Swedish) Check your e-mail"
-                                   , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                           Click the button in the email to log in."""
-                                   }
-                   }
-    , form5 =
-                       { emailAddressNotValid = "(Swedish) This email is not valid"
-                       , emailAddressText = "(Swedish) E-mail"
-                       , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                       }
-                   , success =
-                       { checkYourEmail = "(Swedish) Check your e-mail"
-                       , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-               Click the button in the email to log in."""
-                       , form3 =
-                                       { emailAddressNotValid = "(Swedish) This email is not valid"
-                                       , emailAddressText = "(Swedish) E-mail"
-                                       , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                                       }
-                                   , success =
-                                       { checkYourEmail = "(Swedish) Check your e-mail"
-                                       , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                               Click the button in the email to log in."""
-                                       }
-                       }
-   , form6 =
-                          { emailAddressNotValid = "(Swedish) This email is not valid"
-                          , emailAddressText = "(Swedish) E-mail"
-                          , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                          }
-                      , success =
-                          { checkYourEmail = "(Swedish) Check your e-mail"
-                          , emailSent = \email -> "(Swedish) We have sent an e-mail to **" ++ Email.toString email ++ """**.
-
-                  Click the button in the email to log in."""
-                          , form3 =
-                                          { emailAddressNotValid = "(Swedish) This email is not valid"
-                                          , emailAddressText = "🚧"
-                                          , termsOfService = \urls -> "(Swedish) By clicking on **Log in** you agree to the use of cookies. Read more in our [complete cookie policy](" ++ urls.termsOfService ++ ")."
-                                          }
-                                      , success =
-                                          { checkYourEmail = ""
-                                          , emailSent = \email -> Email.toString email
-                                          }
-                          }
     }
